37ce33324466439ffac615cb424c921eb9f8a3fe
trippindang <144600679+trippindang@users.noreply.github.com>
2026-05-13 12:07:36 +0700

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,8 +1,20 @@
-const cacheName = 'silent-guard-v1';
+const cacheName = 'campus-guard-v2';
 const assets = ['./', './index.html', './manifest.json'];
 
 self.addEventListener('install', e => {
+  self.skipWaiting();
   e.waitUntil(caches.open(cacheName).then(cache => cache.addAll(assets)));
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(keys => {
+      return Promise.all(
+        keys.filter(key => key !== cacheName).map(key => caches.delete(key))
+      );
+    })
+  );
+  return self.clients.claim();
 });
 
 self.addEventListener('fetch', e => {
